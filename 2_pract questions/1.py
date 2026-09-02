086a14a6c9effce03da986709338fec63c627410
Abhishek5499 <abhishekganesh903@gmail.com>
2026-09-02 12:45:56 +0530

    1.py

---
+++ 2_pract questions/1.py
@@ -0,0 +1,10 @@
+# Create a NumPy array containing 10, 20, 30, 40 and 50. Print the array, its number of dimensions, shape, size and data type..
+
+import numpy as np
+
+a1 = np.array([10,20,30,40,50])
+
+print(a1)
+print(a1.ndim)
+print(a1.shape)
+print(a1.size)
